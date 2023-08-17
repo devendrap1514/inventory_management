85190a50ac794d583c303f01ba4395402312de6f
Devendra Shriffle <devendrap@shriffle.com>
2023-08-17 10:34:06 +0530

    remove numeric validation from product and add to length and width

--- app/models/product_variant.rb
+++ app/models/product_variant.rb
@@ -2,6 +2,7 @@ class ProductVariant < ApplicationRecord
   belongs_to :product
   has_many :cards
 
-  validates :product, presence: true, numericality: { only_integer: true }
-  validates :length, :width, presence: true, numericality: { only_integer: true }
+  validates :product, presence: true
+  validates :length, presence: true, numericality: { only_integer: true }
+  validates :width, presence: true, numericality: { only_integer: true }
 end
