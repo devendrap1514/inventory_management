ca3dc057c52f2d66dea0509165f1d8bafd94ff9d
Devendra Shriffle <devendrap@shriffle.com>
2023-08-17 14:42:15 +0530

    change scope method name

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
 
   default_scope { order(:name) }
 
-  scope :search, ->(text) {
+  scope :search_product, ->(text) {
     ActiveRecord::Base.connection.execute(
       "SELECT pv.id AS 'product variant id', p.name, p.brand_name, pv.length, pv.width FROM products AS p INNER JOIN product_variants AS pv
       ON p.id = pv.product_id WHERE name LIKE '#{text}%'")
