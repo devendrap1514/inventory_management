7db00bbd00a810ccae44ec6e6a191ce67f582e8f
Devendra Shriffle <devendrap@shriffle.com>
2023-08-17 10:24:03 +0530

    change numerically to numericality and delete some unknown file which are added after app update and add email validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,14 @@ class Customer < ApplicationRecord
 
   validates :first_name, presence: { message: "First name can't be empty" }
   validates :last_name, presence: { message: "Last name can't be empty" }
-  validates :email, presence: true, uniqueness: { case_sensitive: false }  # defualt message will be "Email has already been taken"
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /[a-zA-Z]+[0-9]*[a-zA-Z]*@[a-zA-Z]+\.com/ }
 
+  before_validation :remove_whitespace
+
+  def remove_whitespace
+    self.first_name = self.first_name.strip.squeeze(" ")
+    self.last_name = self.last_name.strip.squeeze(" ")
+    self.email = self.email.gsub(/\s+/, "")
+  end
 
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -4,14 +4,14 @@ class Vendor < ApplicationRecord
   validates :first_name, presence: { message: "First name can't be empty" }
   validates :last_name, presence: { message: "Last name can't be empty" }
   # defualt message will be "Email has already been taken"
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /[a-zA-Z]+[0-9]*[a-zA-Z]*@[a-zA-Z]+\.com/ }
 
-  before_validation :strip
+  before_validation :remove_whitespace
 
-  def strip
+  def remove_whitespace
     self.first_name = self.first_name.strip.squeeze(" ")
     self.last_name = self.last_name.strip.squeeze(" ")
-    self.email = self.email.strip.squeeze(" ")
+    self.email = self.email.gsub(/\s+/, "")
   end
 
 end
