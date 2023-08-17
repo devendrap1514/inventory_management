7114e77a6e523119f148073f0aded9f34bf565ff
Devendra Shriffle <devendrap@shriffle.com>
2023-08-17 09:58:25 +0530

    add integer validation in product_variant.rb model

--- app/models/product_variant.rb
+++ app/models/product_variant.rb
@@ -2,6 +2,6 @@ class ProductVariant < ApplicationRecord
   belongs_to :product
   has_many :cards
 
-  validates :product, presence: true
-  validates :length, :width, presence: true
+  validates :product, presence: true, numerically: { only_integer: true }
+  validates :length, :width, presence: true, numerically: { only_integer: true }
 end
